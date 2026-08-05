e3e370b4a2637784a77b6d8ae21d1f99056517ba
Rishabh <95174850+Rishabh-Codes27@users.noreply.github.com>
2026-08-05 19:49:47 +0530

    Add: average-waiting-time - submission-0

---
+++ Data Structures & Algorithms/average-waiting-time/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+
+        current_time = 0
+        total_waiting_time = 0
+
+        for arrival_time, cooking_time in customers:
+            if current_time < arrival_time:
+                current_time = arrival_time
+
+            current_time += cooking_time
+            total_waiting_time += current_time - arrival_time
+        
+        return total_waiting_time / len(customers)
+        
